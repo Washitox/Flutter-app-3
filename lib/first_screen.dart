6fc781553492ee68ff325159003106a9a470e754
Washington <washovilla78@gmail.com>
2024-07-05 16:00:27 -0500

    Flutter entrada

---
+++ lib/first_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'second_screen.dart';
+
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void _login() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username == 'admin' && password == '123') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              SecondScreen(data: 'Hola desde la Primera Pantalla'),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Credenciales incorrectas')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primera Pantalla'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Usuario'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Login'),
+              onPressed: _login,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
